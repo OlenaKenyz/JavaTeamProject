a7573483bf365ad581d5650e2ab0841f66f87c1e
Lidiya Zhhuta <lidiazhhuta@Lidyia-Zhhutas-MacBook-Pro.local>
2015-09-04 19:53:45 +0300

    Added JsonParser.parse() method

--- src/com/team1/project/JsonParser.java
+++ src/com/team1/project/JsonParser.java
@@ -1,5 +1,26 @@
 package com.team1.project;
 
-public class JsonParser {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+public class JsonParser {
+	public static List<Song> parse(String fileName) throws JsonParseException, JsonMappingException, IOException {
+
+		InputStream input = new FileInputStream(fileName);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		objectMapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+
+		List<Song> songs = objectMapper.readValue(input, Songs.class).getSongs();
+		
+		return songs
+	}
+}
 }
